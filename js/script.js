56a36e052f42cdb98cf1cf12f1f5b7858b78483c
PizzaMyHeart <ian_tan@hotmail.com>
2021-02-20 17:37:23 +0000

    cleaned up javascript

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-
-
+// **************
+// Modal overlay*
+// **************
 const books = document.getElementsByClassName('books-container-item');
 
 const modal = document.getElementById('modal');
 
+// Display overlay when clicking book thumnail
 for(var i = 0; i < books.length; i++) {
     let book = books[i];
     book.onclick = function() {
@@ -14,52 +16,21 @@ for(var i = 0; i < books.length; i++) {
     }
 }
 
+// Double-click to close overlay
 modal.addEventListener('dblclick', function() {
 	modal.style.display = 'none';
 	modal.innerHTML = '';
 });
 
 
-const allKeywords = [];
-// Insert tags as class names
-for(var i = 0; i < books.length; i++) {
-	let book = books[i];
-	let keywords = book.querySelectorAll('.keyword');
-	if (keywords !== null) {
-		for(var j = 0; j < keywords.length; j++) {
-			let keyword = keywords[j].innerHTML;
-			// Store in allKeywords for use in constructing Isotope filter
-			allKeywords.push(keyword);
-			// Convert spaces to dashes, to produce valid class names
-			keyword = keyword.replace(/\s+/g, '-').replace(/,/g, '');
-			book.classList.add(keyword);
 
-		};
-	};
-};
+// ********
+// Isotope*
+// ********
 
-const yearFilter = document.querySelector('#year-filter')
-
-// Create keyword filter list
-const keywordFilter = document.querySelector('#keyword-filter');
-let keywordsUnique = Array.from(new Set(allKeywords)).sort();
-console.log(keywordsUnique);
-
-for(var i = 0; i < keywordsUnique.length; i++) {
-	var keyword = keywordsUnique[i];
-	var optionNode = document.createElement('option');
-	// Prepend with '.' for use as Isotope filter value
-	optionNode.value = '.' + keyword.replace(/\s+/g, '-').replace(/,/g, '');
-	optionNode.innerHTML = keyword;
-	keywordFilter.appendChild(optionNode);
-
-}
-
-// Isotope
-
-var elem = document.querySelector('.books-container');
+const elem = document.querySelector('.books-container');
 const filtersSelectAll = document.querySelectorAll('.filters-select');
-let filters = {};
+var filters = {};
 // Reset button
 const resetBtn = document.querySelector('#reset');
 resetBtn.addEventListener('click', reset);
@@ -116,12 +87,51 @@ var iso = initIso();
 
 // flatten object by concatting values
 function concatValues( obj ) {
-	console.log(obj)
-  var value = '';
-  for ( var prop in obj ) {
-    value += obj[ prop ];
-  }
-  return value;
+	console.log(obj);
+  	var value = '';
+  	for ( var prop in obj ) {
+    	value += obj[ prop ];
+  	};
+  	return value;
+};
+
+
+// Insert tags as class names in each book item
+const allKeywords = [];
+
+for(var i = 0; i < books.length; i++) {
+	let book = books[i];
+	let keywords = book.querySelectorAll('.keyword');
+	if (keywords !== null) {
+		for(var j = 0; j < keywords.length; j++) {
+			let keyword = keywords[j].innerHTML;
+			// Store in allKeywords for use in constructing Isotope selectors
+			allKeywords.push(keyword);
+			// Convert spaces to dashes, to produce valid class names
+			keyword = keyword.replace(/\s+/g, '-').replace(/,/g, '');
+			book.classList.add(keyword);
+
+		};
+	};
+};
+
+// Create keyword filter list
+
+const yearFilter = document.querySelector('#year-filter')
+
+
+const keywordFilter = document.querySelector('#keyword-filter');
+var keywordsUnique = Array.from(new Set(allKeywords)).sort();
+console.log(keywordsUnique);
+
+for(var i = 0; i < keywordsUnique.length; i++) {
+	var keyword = keywordsUnique[i];
+	var optionNode = document.createElement('option');
+	// Prepend with '.' for use as Isotope filter value
+	optionNode.value = '.' + keyword.replace(/\s+/g, '-').replace(/,/g, '');
+	optionNode.innerHTML = keyword;
+	keywordFilter.appendChild(optionNode);
+
 }
 
 const impossible = document.querySelector('#impossible');
@@ -135,48 +145,3 @@ function impossibleOff() {
 }
 
 
-
-
-
-/*
-function bindFilters(iso) {
-	for(var i = 0; i < filtersSelectAll.length; i++) {
-		var filtersSelect = filtersSelectAll[i];
-		filtersSelect.onchange = function(event) {
-			var select = event.target;
-			var filterGroup = select.getAttribute('data-filter-group');
-			filters[ filterGroup ] = event.target.value;
-			var filterValue = concatValues(filters);
-			iso.arrange({ filter: filterValue });
-		}
-	};
-	return iso;
-}
-*/
-
-// Create Isotope object
-
-/*
-if (/list/.test(window.location.href)) {
-	var iso = new Isotope(elem, {
-		itemSelector: '.books-container-item',
-		layoutMode: 'vertical'
-	});
-} else {
-	var iso = new Isotope(elem, {
-		itemSelector: '.books-container-item',
-	});
-};
-
-for(var i = 0; i < filtersSelectAll.length; i++) {
-	var filtersSelect = filtersSelectAll[i];
-	filtersSelect.onchange = function(event) {
-		var select = event.target;
-		var filterGroup = select.getAttribute('data-filter-group');
-		filters[ filterGroup ] = event.target.value;
-		var filterValue = concatValues(filters);
-		iso.arrange({ filter: filterValue });
-	}
-}
-
-*/
